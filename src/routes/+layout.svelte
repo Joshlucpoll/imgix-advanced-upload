d6b2127c1e8b237515346b48f6d6f928e8501f32
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2023-05-12 23:43:43 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,87 @@
+<script>
+  import "../app.css";
+</script>
+
+<div class="min-h-screen flex flex-col justify-between">
+  <nav class="py-8 flex flex-row justify-end items-center">
+    <a href="https://github.com/joshlucpoll/imgix-advanced-upload">
+      <svg
+        class="opacity-80"
+        xmlns="http://www.w3.org/2000/svg"
+        x="0px"
+        y="0px"
+        width="40"
+        height="40"
+        viewBox="0 0 30 30"
+        fill="white"
+      >
+        <path
+          d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"
+        />
+      </svg>
+    </a>
+  </nav>
+  <slot />
+
+  <footer class="pb-8 flex flex-row justify-end items-center self-end">
+    <small class="opacity-60"
+      >icons by <a class="underline" href="https://icons8.com">Icons8</a> • made
+      by
+      <a class="underline" href="https://joshlucpoll.dev">Joshlucpoll</a></small
+    >
+  </footer>
+</div>
+
+<svg
+  class="fixed top-0 left-0 w-full -z-10"
+  viewBox="0 0 1442 1442"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <g clip-path="url(#clip0_1_2)" filter="url(#filter0_f_1_2)">
+    <rect
+      width="1042"
+      height="1042"
+      transform="translate(200 200)"
+      fill="black"
+    />
+    <path
+      d="M1139 676.5C1139 756.402 1118.23 834.93 1078.74 904.365C1039.24 973.8 982.381 1031.75 913.741 1072.53C845.1 1113.3 767.042 1135.49 687.239 1136.93C607.436 1138.36 528.635 1118.98 458.579 1080.69L679 676.5H1139Z"
+      fill="#E74A4A"
+      fill-opacity="0.7"
+    />
+    <path
+      d="M1103.9 352.64C1006.44 297.747 894.375 274.394 783.105 285.792C671.835 297.189 566.829 342.777 482.525 416.286C398.22 489.796 338.759 587.617 312.317 696.299C285.875 804.981 293.751 919.185 334.862 1023.21L838.26 824.26L1103.9 352.64Z"
+      fill="#3634C5"
+      fill-opacity="0.7"
+    />
+  </g>
+  <defs>
+    <filter
+      id="filter0_f_1_2"
+      x="0"
+      y="0"
+      width="1442"
+      height="1442"
+      filterUnits="userSpaceOnUse"
+      color-interpolation-filters="sRGB"
+    >
+      <feFlood flood-opacity="0" result="BackgroundImageFix" />
+      <feBlend
+        mode="normal"
+        in="SourceGraphic"
+        in2="BackgroundImageFix"
+        result="shape"
+      />
+      <feGaussianBlur stdDeviation="100" result="effect1_foregroundBlur_1_2" />
+    </filter>
+    <clipPath id="clip0_1_2">
+      <rect
+        width="1042"
+        height="1042"
+        fill="white"
+        transform="translate(200 200)"
+      />
+    </clipPath>
+  </defs>
+</svg>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,67 @@
+<main class="flex flex-col justify-center items-center">
+  <h1 class="text-center font-bold text-4xl pb-2">
+    Welcome to <span>imgix</span> Advanced Upload
+  </h1>
+  <p class="text-center opacity-60 max-w-xl">
+    This tool allows for more complex image uploads by automatically
+    transferring existing <a href="https://en.wikipedia.org/wiki/Exif">Exif</a> image
+    metadata to imgix custom fields
+  </p>
+
+  <div class="mt-24 w-full max-w-xl flex flex-row items-center gap-8">
+    <input
+      id="api-input"
+      class="p-2 px-4 text-center bg-[#1D1A2488] rounded-md w-full outline outline-slate-400"
+      type="password"
+      placeholder="API Key"
+    />
+    <button
+      type="button"
+      on:click={() => {
+        var x = document.getElementById("api-input");
+        if (x.type === "password") {
+          x.type = "text";
+        } else {
+          x.type = "password";
+        }
+      }}
+    >
+      <svg
+        fill="#fffB"
+        height="24px"
+        width="24px"
+        version="1.1"
+        id="Capa_1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        viewBox="0 0 488.85 488.85"
+        xml:space="preserve"
+      >
+        <g>
+          <path
+            d="M244.425,98.725c-93.4,0-178.1,51.1-240.6,134.1c-5.1,6.8-5.1,16.3,0,23.1c62.5,83.1,147.2,134.2,240.6,134.2
+       s178.1-51.1,240.6-134.1c5.1-6.8,5.1-16.3,0-23.1C422.525,149.825,337.825,98.725,244.425,98.725z M251.125,347.025
+       c-62,3.9-113.2-47.2-109.3-109.3c3.2-51.2,44.7-92.7,95.9-95.9c62-3.9,113.2,47.2,109.3,109.3
+       C343.725,302.225,302.225,343.725,251.125,347.025z M248.025,299.625c-33.4,2.1-61-25.4-58.8-58.8c1.7-27.6,24.1-49.9,51.7-51.7
+       c33.4-2.1,61,25.4,58.8,58.8C297.925,275.625,275.525,297.925,248.025,299.625z"
+          />
+        </g>
+      </svg>
+    </button>
+  </div>
+
+  <!-- <div class="my-12 w-full flex flex-row gap-8 items-center justify-center">
+    <h2>Source</h2>
+    <select
+      class="p-2 px-4 text-center bg-[#1D1A24] rounded-md w-60 outline outline-[#803527]"
+      disabled
+    >
+      <option value="" disabled selected>enter API key</option>
+    </select>
+  </div> -->
+
+  <button
+    class="mt-24 py-3 px-8 bg-white text-black font-bold rounded-md shadow-[0_0_50px_-7px_rgba(255,255,255,1)] hover:shadow-[0_0_100px_-2px_rgba(255,255,255,1)] hover:scale-105 transition-all"
+    type="button">Select Images</button
+  >
+</main>
